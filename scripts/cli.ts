226c8c4536fb39ae32398c89ff94b1e5d56bba15
Mitchell Slager <mslager92@gmail.com>
2023-01-20 13:57:02 +0100

    feat: add constants file

--- scripts/cli.ts
+++ scripts/cli.ts
@@ -1,24 +1,26 @@
 /**
- * Command-line interface script
+ * Rush Hour scraper
+ * Scrapes the latest releases from https://www.rushhour.nl
  */
 import { parse } from 'node-html-parser'
 import chalk from 'chalk'
+import { selectors, urls } from './constants'
 
 const fetchNewThisWeek = async () => {
   try {
     // Fetch URL and parse HTML with node-html-parser
-    const res = await fetch('https://www.rushhour.nl/new-this-week')
+    const res = await fetch(urls.newThisWeek)
     const body = await res.text()
     const parsed = parse(body)
 
     // Select all relevant items on the page
-    const view = parsed.querySelector('.record-grid-view > .view-content')
-    const items = view?.querySelectorAll('.node-record')
+    const view = parsed.querySelector(selectors.view)
+    const items = view?.querySelectorAll(selectors.item)
 
     // Log the artist and album title in the console
     items?.forEach((item) => {
-      const artist = item.querySelector('.field-name-field-artist')?.text.toString()
-      const title = item.querySelector('.field-name-title')?.text.toString()
+      const artist = item.querySelector(selectors.artist)?.text.toString()
+      const title = item.querySelector(selectors.title)?.text.toString()
       console.log(`${chalk.blue(artist)} - ${chalk.red(title)}`)
     })
   } catch (error) {
@@ -26,11 +28,11 @@ const fetchNewThisWeek = async () => {
   }
 }
 
-// const getPageContent = async (url: string) => {
-//   try {
-//   } catch (err) {
-//     console.log(err)
-//   }
-// }
+const getPageContent = async (url: string) => {
+  try {
+  } catch (err) {
+    console.log(err)
+  }
+}
 
 fetchNewThisWeek()
---
+++ scripts/constants.ts
@@ -0,0 +1,10 @@
+export const urls = {
+  newThisWeek: 'https://www.rushhour.nl/new-this-week',
+}
+
+export const selectors = {
+  view: '.record-grid-view > .view-content',
+  item: '.node-record',
+  artist: '.field-name-field-artist',
+  title: '.field-name-title',
+}
